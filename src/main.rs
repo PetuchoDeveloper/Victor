1004d008472a3926fee6e587d27f728b3fde3c82
PetuchoDeveloper <kevin.varick@gmail.com>
2026-04-16 19:19:05 -0700

    fix: type annotations, debug logic (now compiles)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+//if models arent imported compiler ignores them
+mod models;
 mod vmath;
+mod storage;
+mod engine;
+
 fn main() {
     //this searches for mod.rs files in subfolders wich is a cool way to import them, nice rust!
     use vmath::euclidian_distance;
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,10 +1,11 @@
-use models::Point;
+use crate::models::Point;
 use std::collections::HashMap;
-use vmath::{euclidian_distance, cosine_similarity};
+use crate::vmath::{euclidian_distance, cosine_similarity};
 use uuid::Uuid;
 use tokio::fs;
 
 //in rust behavior and data are separated, the struct defines the shape of the object: what does it contain?
+#[derive(Clone, Debug)]
 pub struct Collection {
     pub name: String,
     pub dimension: usize,
@@ -14,24 +15,24 @@ pub struct Collection {
 //while an implementation defines the behavior of the object, it can define default values for the structure and what methods are available to read or mutate the data inside the structure: what it does?
 impl Collection {
     //this, besides being the constructor, is also called an associated function, these dont take self as an argument but are called directly on the struct type, these are usually used to create new instances of the struct
-    pub fn new() -> Self {
+    pub fn new(name: String, dimension: usize) -> Self {
         Collection {
-            name: String::new(),
-            dimension: usize::default(),
+            name: name,
+            dimension: dimension,
             metric: String::new(),
             hmap: HashMap::new(),
         }
     }
     //these are methods, they can define if they mutate the structure somehow or if they are read-only, but regardless they do work, like standard methods from other languages
-    pub fn upsert(&mut self, point: Point) -> Result<(), String> {
-        if(point.vector.len() != self.dimension) {
+    pub fn upsert(&mut self, point: Point) -> Result<&str, &str> {
+        if point.vector.len() != self.dimension {
             return Err("Vector dimension does not match collection dimension");
         }
         self.hmap.insert(point.id, point);
-        return Ok(());
+        return Ok("vector upserted");
     }
-    pub fn search(&self, query_vector: Vec<f32>, k: usize) -> Result<Vec<Point>, String> {
-        if(query_vector.len() != self.dimension) {
+    pub fn search(&self, query_vector: Vec<f32>, k: usize) -> Result<Vec<Point>, &str> {
+        if query_vector.len() != self.dimension {
             return Err("Vector dimension does not match collection dimension");
         }
         let mut distances = Vec::new();
@@ -62,17 +63,17 @@ impl Collection {
         }
 
         distances.truncate(k);
-        distances.into_iter().map(|(id, _)| self.hmap.get(&id).unwrap().clone()).collect()
-
-        return Ok(distances);
+        println!("{:?}", distances);
+        let results: Vec<Point> = distances.into_iter().map(|(id, _)| self.hmap.get(&id).unwrap().clone()).collect::<Vec<Point>>();
+        return Ok(results);
     }
 
-    pub async fn save_to_disk(&self, filepath: &str) -> Result<(), String> {
+    pub async fn save_to_disk(&self, filepath: &str) -> Result<&str, String> {
         let serialized_data = serde_json::to_string(&self.hmap).map_err(|e| e.to_string())?;
 
         fs::write(filepath, serialized_data).await.map_err(|e| e.to_string())?;
 
-        return Ok(());
+        return Ok("vectors saved to disk");
     }
         
 }
